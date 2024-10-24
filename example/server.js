09d637aa5177db631e4b01ebab34dea8019eb771
qgustavor <qgustavor@users.noreply.github.com>
2024-10-24 08:51:16 -0300

    Project clean up

--- example/server.js
+++
@@ -1,8 +0,0 @@
-const later = require('..'); // require('@breejs/later') if installed via npm
-
-// create the desired schedule
-const sched = later.parse.text('every 5 mins on the 30th sec');
-
-// calculate the next 5 occurrences using local time
-later.date.localTime();
-const results = later.schedule(sched).next(5);
